4cb81ad5fd6edc7462e8b048c4fe15d8a356068e
noheropage <mursepage@gmail.com>
2021-03-08 22:50:03 -0800

    Create port for server.js

---
+++ public/assets/js/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// TODO: need routing JS?
+// require('.routes/apiRoutes')(app) 
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
