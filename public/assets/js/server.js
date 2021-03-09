7cbf77c6ec1cdb3aad7e6a788f58232fd51dd444
noheropage <mursepage@gmail.com>
2021-03-08 23:09:21 -0800

    Create routing for html

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -8,7 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // TODO: need routing JS?
-// require('.routes/apiRoutes')(app) 
+require('.routes/apiRoutes')(app)
+require('.routes/htmlRoutes')(app)
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
