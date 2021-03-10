591f9470b3b9e214095a8267949fa91ca25cc7fc
noheropage <mursepage@gmail.com>
2021-03-10 11:38:25 -0800

    Create unique id for each note saved

--- db/routes/apiRoutes.js
+++ db/routes/apiRoutes.js
@@ -1,11 +1,22 @@
 const db = require('../db.json')
+const crypto = require('crypto')
+
+const generateUniqueID = () => {
+        return crypto.randomBytes(8).toString('hex')
+    }
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(db));
 
     app.post('/api/notes', (req, res) => {
+        req.id = generateUniqueID();
+        req.body['id'] = req.id
         db.push(req.body)
         res.json(true)
     })
 
+    app.delete('/api/notes', (req, res) => {
+        
+    })
+    // TODO: delete function?
 }
--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-// TODO: need routing JS?
-require('.routes/apiRoutes')(app)
-require('.routes/htmlRoutes')(app)
+require('./db/routes/apiRoutes')(app)
+require('./db/routes/htmlRoutes')(app)
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
